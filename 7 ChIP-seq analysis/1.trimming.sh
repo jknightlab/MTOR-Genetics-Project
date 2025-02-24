a1c7a2fbb053adf30e27d62b0567b2d6f057f54d
Ping Zhang <53820716+zhang-ping-wj@users.noreply.github.com>
2025-02-24 11:48:58 +0000

    Update 1.trimming.sh

--- 7 ChIP-seq analysis/1.trimming.sh
+++ 7 ChIP-seq analysis/1.trimming.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH -A jknight.prj 
-#SBATCH -p short --cpus-per-task 4     # Partition (queue) - asking for short queue & 4-cpu job
-#SBATCH -J trmming             # Job name
-#SBATCH -o triming-%a.o          # Standard error "slurm-%A_%a.out", "%A" is replaced by the job ID and "%a" with the array index.
+#SBATCH -A {}.prj 
+#SBATCH -p short --cpus-per-task 4
+#SBATCH -J trmming
+#SBATCH -o triming-%a.o
 #SBATCH --array 1-12:1 
 
 echo "------------------------------------------------"
@@ -18,32 +18,17 @@ mkdir Mapping
 fi
 
 ## SE Fastq files 
-##FASTQDIR="/well/jknight/users/kwz374/MTOR-genetic-project/CD4+.T.cell.5mC-and-5hmC.medIP-Seq/fastq.file"
-##FASTQDIR="/well/jknight/users/kwz374/MTOR-genetic-project/CD4+.T.cell.H3K9me3-and-KAP1-ChIP-seq/fastq.file"
+FASTQDIR="/well/jknight/users/kwz374/MTOR-genetic-project/CD4+.T.cell.5mC-and-5hmC.medIP-Seq/fastq.file"
 
-#echo "Fastq directory: "$FASTQDIR
-
-#FASTQ=$(cat sample.key.txt | tail -n+${SLURM_ARRAY_TASK_ID} | head -1 | cut -f1 )
-#echo $FASTQ
-#FASTQ=$FASTQDIR/$FASTQ".fastq.gz"
-
-#module load Trim_Galore/0.6.2-GCCcore-8.2.0-Java-11
-
-#trim_galore --cores 16 --gzip --output_dir Mapping/AdapterTrimmed/ $FASTQ
-
-# PE Fastq files 
-FASTQDIR="/well/jknight/users/kwz374/MTOR-genetic-project/CD4+.T.cell.5hmC.medIP-Seq_rep2_PRJEB13837/fastq.file"
 echo "Fastq directory: "$FASTQDIR
 
 FASTQ=$(cat sample.key.txt | tail -n+${SLURM_ARRAY_TASK_ID} | head -1 | cut -f1 )
 echo $FASTQ
-FASTQ1=${FASTQDIR}/${FASTQ}_1.fastq.gz
-FASTQ2=${FASTQDIR}/${FASTQ}_2.fastq.gz
-READS=$FASTQ1" "$FASTQ2
+FASTQ=$FASTQDIR/$FASTQ".fastq.gz"
 
-module load Trim_Galore/0.6.10-GCCcore-12.3.0
+module load Trim_Galore/0.6.2-GCCcore-8.2.0-Java-11
 
-trim_galore --cores 16 --paired --gzip --output_dir Mapping/AdapterTrimmed/ $READS
+trim_galore --cores 16 --gzip --output_dir Mapping/AdapterTrimmed/ $FASTQ
 
 echo "Ended at: "`date`
 
