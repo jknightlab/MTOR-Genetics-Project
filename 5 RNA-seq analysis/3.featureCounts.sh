ed8e447db2cedf34e9cf45975d5c20228c2607d5
Ping Zhang <53820716+zhang-ping-wj@users.noreply.github.com>
2025-02-24 11:40:24 +0000

    Update 3.featureCounts.sh

--- 5 RNA-seq analysis/3.featureCounts.sh
+++ 5 RNA-seq analysis/3.featureCounts.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH -A jknight.prj 
+#SBATCH -A {}.prj 
 #SBATCH -p short --cpus-per-task 8
 #SBATCH -J featurecount            
-#SBATCH -o featurecount-%a.o          # Standard error "slurm-%A_%a.out", "%A" is replaced by the job ID and "%a" with the array index.
+#SBATCH -o featurecount-%a.o
 
 echo "Started at: "`date`
 
@@ -11,7 +11,7 @@ if [[ ! -e featureCounts ]]; then
 mkdir -p featureCounts
 fi
 
-GTF=/well/jknight/users/kwz374/gtfs/gencode.v31.annotation.gtf.gz
+GTF=/gtfs/gencode.v31.annotation.gtf.gz
 
 /apps/htseq/subread-1.6.4-Linux-x86_64/bin/featureCounts -T 12 \
 -p -s 2 -a $GTF -g gene_name \
